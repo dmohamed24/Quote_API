a3c995cf04ab69dd8cf021f3a45146d2e601d235
Dahir Mohamed <dahir.mohamedd24@gmail.com>
2025-02-16 15:06:11 +0000

    QUOTE-API-2: added a new /quotes api endpoint

--- src/index.js
+++ src/index.js
@@ -16,6 +16,10 @@ app.get('/quote', (req, res) => {
   res.json(randomQuote)
 })
 
+app.get('/quotes', (req, res) => {
+  res.json(quotes)
+})
+
 if (require.main === module) {
   const PORT = process.env.PORT || 3000
   app.listen(PORT, () => console.log(`Server running on localhost:${PORT}`))
