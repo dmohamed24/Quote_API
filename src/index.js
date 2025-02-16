8863ce7af88ad60596dd85f3b89301e3fded43e3
Dahir Mohamed <dahir.mohamedd24@gmail.com>
2025-02-16 12:44:12 +0000

    QUOTE-API-1: fixed api, linting and tests

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 3000;
+const express = require('express')
+const app = express()
 
 const quotes = [
-  { id: 1, text: "The purpose of our lives is to be happy." },
-  { id: 2, text: "Life is what happens when you’re busy making other plans." },
-  { id: 3, text: "Get busy living or get busy dying." },
-];
+  { id: 1, text: 'The purpose of our lives is to be happy.' },
+  { id: 2, text: 'Life is what happens when you’re busy making other plans.' },
+  { id: 3, text: 'Get busy living or get busy dying.' }
+]
 
-app.get("/", (req, res) => {
-  res.send("Welcome to the Quote API!");
-});
+app.get('/', (req, res) => {
+  res.send('Welcome to the Quote API!')
+})
 
-app.get("/quote", (req, res) => {
-  const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
-  res.json(randomQuote);
-});
+app.get('/quote', (req, res) => {
+  const randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
+  res.json(randomQuote)
+})
 
-app.listen(PORT, () => console.log(`Server running on localhost:${PORT}`));
+if (require.main === module) {
+  const PORT = process.env.PORT || 3000
+  app.listen(PORT, () => console.log(`Server running on localhost:${PORT}`))
+}
 
-module.exports = app;
+module.exports = app
