00e0308138112e621fa061aaa91c5c13144a6204
Dahir Mohamed <dahir.mohamedd24@gmail.com>
2025-02-16 16:25:36 +0000

    QUOTE-API-2: added a new quote

--- src/index.js
+++ src/index.js
@@ -1,28 +1,32 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 const quotes = [
-  { id: 1, text: 'The purpose of our lives is to be happy.' },
-  { id: 2, text: 'Life is what happens when you’re busy making other plans.' },
-  { id: 3, text: 'Get busy living or get busy dying.' }
-]
+  { id: 1, text: "The purpose of our lives is to be happy." },
+  { id: 2, text: "Life is what happens when you’re busy making other plans." },
+  { id: 3, text: "Get busy living or get busy dying." },
+  {
+    id: 4,
+    text: "Nothing is impossible. The word itself says 'I'm possible'!",
+  },
+];
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the Quote API!')
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to the Quote API!");
+});
 
-app.get('/quote', (req, res) => {
-  const randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
-  res.json(randomQuote)
-})
+app.get("/quote", (req, res) => {
+  const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+  res.json(randomQuote);
+});
 
-app.get('/quotes', (req, res) => {
-  res.json(quotes)
-})
+app.get("/quotes", (req, res) => {
+  res.json(quotes);
+});
 
 if (require.main === module) {
-  const PORT = process.env.PORT || 3000
-  app.listen(PORT, () => console.log(`Server running on localhost:${PORT}`))
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => console.log(`Server running on localhost:${PORT}`));
 }
 
-module.exports = app
+module.exports = app;
